9ce7589b37060758eb38c332561c2394f605527e
Ian Salazar <ian.salazar@itriad.org.br>
2022-12-13 00:32:49 -0400

    refactor: add mysql and initial entities

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,13 +1,21 @@
 import 'reflect-metadata';
+import 'dotenv/config';
+import { AppDataSource } from '../../../data-source';
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
-const app = express();
+AppDataSource.initialize()
+  .then(() => {
+    const app = express();
 
-app.use(cors());
-app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
 
-app.listen(3333, () =>
-  console.log('🚀 Server rodando! Acesse: http://localhost:3333/'),
-);
+    return app.listen(process.env.API_PORT as unknown as number | 3333, () =>
+      console.log(
+        `🚀 Server rodando! Acesse: http://localhost:${process.env.API_PORT}`,
+      ),
+    );
+  })
+  .catch((error) => console.log(error));
