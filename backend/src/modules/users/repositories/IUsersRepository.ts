28a065050ea7fd09bf3f1091eb250ebbe6731e59
Ian Salazar <ian.salazar@itriad.org.br>
2022-12-14 22:15:07 -0400

    feat: add login and create user

---
+++ backend/src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,10 @@
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import User from '../infra/typeorm/entities/User';
+
+export default interface IUsersRepository {
+  findById(id: number): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  create(data: ICreateUserDTO): Promise<User>;
+  update(user: User): Promise<User>;
+  delete(user: User): Promise<void>;
+}
---
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import IUserRepository from '../repositories/IUsersRepository';
+import { inject, injectable } from 'tsyringe';
+import IHashProvider from '../providers/HashProvider/interfaces/IHashProvider';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+@injectable()
+export default class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExist = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExist) {
+      throw new AppError('Email address already exists');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
