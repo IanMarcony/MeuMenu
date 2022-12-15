af63e30a23c91fcb26cfa4b7b7f0656563ed0e59
Ian Salazar <ian.salazar@itriad.org.br>
2022-12-14 22:10:36 -0400

    feat: add login and create user

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '7895d135d8c67f8908db131cf1b4f530',
+    expiresIn: '1d',
+  },
+};
---
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export default class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @Column({ type: 'text' })
+  email: string;
+
+  @Column({ type: 'text' })
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
